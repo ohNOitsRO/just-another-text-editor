c1130627a589523d58a8082b189c7012b14fa5fa
Delgado <rxd718@miami.edu>
2023-04-25 14:29:20 -0400

    database work

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
     },
     plugins: [ new HtmlWebpackPlugin({
       template: './index.html',
-      title: 'Webpack Plugin'
+      title: 'J.A.T.E'
     }),
 
     new InjectManifest({
