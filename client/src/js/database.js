ff23d8344241f7ac2081c9797846b98cf2aeb9bf
Delgado <rxd718@miami.edu>
2023-05-01 15:37:48 -0400

    work on getDB function

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log('GET from the J.A.T.E database');
   const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const req = store.getAll();
   const res = await req;
